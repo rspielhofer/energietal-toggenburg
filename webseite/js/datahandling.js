95391cc39d30360ff7f1c7d1f733133852d26c6a
Yannick Suter <yannicksuter@bluewin.ch>
2015-07-10 16:48:21 +0200

    Webseite: Wappen und Gemeindenamen werden für Wattwil, Bütschwil, Ebnat-Kappel und Nesslau angezeigt. Daten werden aus lokaler json-Datei gelesen

---
+++ webseite/js/datahandling.js
@@ -0,0 +1,16 @@
+
+// Use: e.g. getObjects(TestObj, 'id', 'A'), returns array of matching objects
+function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else if (i == key && obj[key] == val) {
+            objects.push(obj);
+        }
+    }
+    return objects;
+}
+
+
